07a603e6c7f571eb9953a9f1ef9689cec94588b5
arimuhamad97 <kedemok98@gmail.com>
2022-08-26 21:09:47 +0800

    test push

--- resources/views/pages/admin/baru.blade.php
+++ resources/views/pages/admin/baru.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('content')
-
+asdd
 <h3 class="mt-14 font-extrabold w-[192px] h-[37px] text-slate-800">Daftar Akun</h3>
 <div class="flex justify-between items-center actionContainer">
     <div class="flex">
