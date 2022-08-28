d7aca3696f78cc77605a5d196e35cba369598532
will1001 <wilirahmatm@gmail.com>
2022-08-28 23:24:15 +0800

    test sinron github

--- resources/views/layouts/member.blade.php
+++ resources/views/layouts/member.blade.php
@@ -23,7 +23,7 @@
   <link rel="apple-touch-icon" sizes="76x76" href="{{ asset('admin_template/assets/img/apple-icon.png') }}">
   <link rel="icon" type="image/png" href="{{ asset('admin_template/assets/img/favicon.png') }}">
   <title>
-   SIMANIS NTB
+   SIMANIS NTB 212
   </title>
   <!--     Fonts and icons     -->
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
