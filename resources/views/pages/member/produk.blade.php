5859eb4b4ccaa8d14909af5e1aa56741580c7312
will1001 <wilirahmatm@gmail.com>
2022-08-28 23:04:32 +0800

    popup member produk

--- resources/views/pages/member/produk.blade.php
+++ resources/views/pages/member/produk.blade.php
@@ -37,7 +37,7 @@
     <td class="text-center p-4 "><span class="bg-halalBgColor text-halalTextColor p-2 rounded-xl">Ada</span></td>
     <td class="text-center p-2"><span class="bg-menungguBgColor text-menungguTextColor p-2 rounded-xl">Ada</span></td>
     <td class="text-center p-2"><span class="bg-disetujuiBgColor text-disetujuiTextColor p-2 rounded-xl">Ada</span></td>
-    <td class="text-center p-4 cursor-pointer"><a href="{{ url('/member/produkDetails') }}" class="text-center text-disetujuiTextColor cursor-pointer">Lihat Detail</a></td>
+    <td onclick="lihatDetails()"  class="text-center p-4 text-disetujuiTextColor cursor-pointer">Lihat Detail</td>
   </tr>
   <tr>
     <td class="text-center p-4 ">2</td>
@@ -67,7 +67,52 @@
     <td class="text-center p-4 text-disetujuiTextColor cursor-pointer">Lihat Detail</td>
   </tr>
  
+ 
+ 
 </table>
 
 
+<div onclick="closeDetails()" style="visibility: collapse;" id="detailPopUpBlackbg" class="bg-black opacity-40 w-full h-full absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 opacity-30">
+</div>
+<div style="visibility: collapse;" id="detailPopUp" class="bg-white rounded-xl popUpContainer absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-50 flex">
+<img class="h-64" src="{{ asset('/img/produk1.png') }}" alt="">
+<div class="p-4">
+    <h3>Sepatu Kain Tenun</h3>
+    <div class="mt-3 mb-3 flex justify-between items-center mr-7"><span>Sertifikat Halal</span> <span class="bg-halalBgColor text-halalTextColor p-2 rounded-xl">Ada</span></div>
+    <div class="mt-3 mb-3 flex justify-between items-center mr-7"><span>Sertifikat HAKI</span> <span class="bg-menungguBgColor text-menungguTextColor p-2 rounded-xl">Ada</span></div>
+    <div class="mt-3 mb-3 flex justify-between items-center mr-7"><span>Sertifikat SNI</span> <span class="bg-disetujuiBgColor text-disetujuiTextColor p-2 rounded-xl">Ada</span></div>
+    <h5>Deskripsi Produk</h5>
+    <p>Sepatu etnik kain tenun bali
+Tampil gaya, trendy dengan produk yang dibuat dengan bahan yg nyaman dipakai.
+Sepatu kain tenun bali tersedia berbagai motif dan warna,
+bahan kain tenun jepara dilapis spon 2 mm + merymesh
+Outsole : PPC
+uk 37 s/d 40
+keterangan ukuran :
+uk 37 = 24 cm
+uk 38 = 24.5 cm
+uk 39 = 25 cm
+uk 40 = 26 cm
+
+sebelum pesan mohon di tanyakan dulu motif dan warna di karenakan motif selalu berubah</p>
+<div class="flex justify-end items-center mt-5">
+      <div class="flex pl-16 pr-16 pt-2 pb-2 bg-buttonColor-900 cursor-pointer text-white rounded-xl font-bold text-xl"><span>Ok</span></div>
+   </div>
+</div>
+</div>
+
 @endsection
+<script>
+  const lihatDetails = ()=>{
+    const blackBg = document.getElementById('detailPopUpBlackbg');
+    const detailPopUp = document.getElementById('detailPopUp');
+    blackBg.style.visibility = "visible";
+    detailPopUp.style.visibility = "visible";
+  }
+  const closeDetails = ()=>{
+    const blackBg = document.getElementById('detailPopUpBlackbg');
+    const detailPopUp = document.getElementById('detailPopUp');
+    blackBg.style.visibility = "collapse";
+    detailPopUp.style.visibility = "collapse";
+  }
+</script>
