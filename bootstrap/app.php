289b67cdb431c027a7863b2156b6f196aea52de5
wiligetsurvey <wili@getsurvey.id>
2022-10-12 18:30:26 +0800

    remove header setting bootsrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,10 +11,7 @@
 |
 */
 
-header('Access-Control-Allow-Credentials', true);
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+
 
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
