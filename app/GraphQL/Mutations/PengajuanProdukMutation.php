dcd12664984bd8b19af1762e505f5e542456e484
will1001 <wilirahmatm@gmail.com>
2022-11-29 21:58:22 +0800

    produk mutaion debug #1

--- app/GraphQL/Mutations/PengajuanProdukMutation.php
+++ app/GraphQL/Mutations/PengajuanProdukMutation.php
@@ -39,30 +39,6 @@ class PengajuanProdukMutation extends Mutation
                 'name' => 'user_id',
                 'type' => Type::nonNull(Type::string()),
             ],
-            'sertifikat_halal_thn' => [
-                'name' => 'sertifikat_halal_thn',
-                'type' => Type::string(),
-            ],
-            'sertifikat_halal_no' => [
-                'name' => 'sertifikat_halal_no',
-                'type' => Type::string(),
-            ],
-            'sertifikat_haki_thn' => [
-                'name' => 'sertifikat_haki_thn',
-                'type' => Type::string(),
-            ],
-            'sertifikat_haki_no' => [
-                'name' => 'sertifikat_haki_no',
-                'type' => Type::string(),
-            ],
-            'sertifikat_sni_thn' => [
-                'name' => 'sertifikat_sni_thn',
-                'type' => Type::string(),
-            ],
-            'sertifikat_sni_no' => [
-                'name' => 'sertifikat_sni_no',
-                'type' => Type::string(),
-            ],
             'nama' => [
                 'name' => 'nama',
                 'type' => Type::nonNull(Type::string()),
@@ -88,34 +64,14 @@ class PengajuanProdukMutation extends Mutation
     {
         $User = User::find($args['user_id']);
 
-        $sertifikat_halal =
-            (object)array(
-                "tahun" => $args["sertifikat_halal_thn"],
-                "nomor" => $args["sertifikat_halal_no"],
-            );
-        $sertifikat_haki =
-            (object)array(
-                "tahun" => $args["sertifikat_haki_thn"],
-                "nomor" => $args["sertifikat_haki_no"],
-            );
-        $sertifikat_sni =
-            (object)array(
-                "tahun" => $args["sertifikat_sni_thn"],
-                "nomor" => $args["sertifikat_sni_no"],
-            );
+       
         $BadanUsaha = BadanUsaha::where('nik', $User->nik)->first();
-        $cek_sertifikat_halal = str_contains(json_encode($sertifikat_halal), "null");
-        $cek_sertifikat_haki = str_contains(json_encode($sertifikat_haki), "null");
-        $cek_sertifikat_sni = str_contains(json_encode($sertifikat_sni), "null");
 
 
 
         $data = array(
             'id' => (string) Str::uuid(),
             'id_badan_usaha' => $BadanUsaha->id,
-            'sertifikat_halal' => $cek_sertifikat_halal ? null : json_encode($sertifikat_halal),
-            'sertifikat_haki' => $cek_sertifikat_haki ? null : json_encode($sertifikat_haki),
-            'sertifikat_sni' => $cek_sertifikat_sni ? null : json_encode($sertifikat_sni),
             'nama' => $args["nama"],
             'harga' => $args["harga"],
             'deskripsi' => $args["deskripsi"],
